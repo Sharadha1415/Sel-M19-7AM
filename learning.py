5b707ba74778ba7c78142e1b9fad1ba0c9041c99
Sharada1415 <ramyar.alpha@gmail.com>
2026-02-27 07:34:47 +0530

    checking even odd for the numbers in the range(2, 50)

--- learning.py
+++ learning.py
@@ -1,9 +1,8 @@
-n = int(input("Enter the value of n : "))
-
-if n%2==0:
-    print(f"even")
-else:
-    print(f"odd")
+for n in range(1, 50):
+    if n%2==0:
+        print(f"{n} is even")
+    else:
+        print(f"{n} is odd")
 
 
 
